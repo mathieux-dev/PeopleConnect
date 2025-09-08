a2da2eed1ea98aafcde8d820f78ca7cf96dfb754
Mateus Mourão <mateusvmourao@gmail.com>
2025-09-08 05:09:44 -0300

    feat: update base path in Vite config and add preview server configuration

--- Frontend/vite.config.ts
+++ Frontend/vite.config.ts
@@ -6,10 +6,13 @@ import { defineConfig } from "vite";
 export default defineConfig({
   plugins: [react()],
   publicDir: "./static",
-  base: "./",
+  base: "/",
   server: {
     port: 3000,
     open: true,
+  },
+  preview: {
+    port: 3000,
   },
   build: {
     outDir: "dist",
